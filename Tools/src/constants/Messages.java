99c9412da77921dc1c0998e3881a535b1ccf3f52
Schloni1995 <33430528+Schloni1995@users.noreply.github.com>
2018-02-28 22:33:25 +0100

    Added Messages

---
+++ Tools/src/constants/Messages.java
@@ -0,0 +1,11 @@
+package constants;
+
+public final class Messages
+{
+	public final static String ESCAPE_MESSAGE="Herunterfahren wurde abgebrochen";
+	public final static String NOT_TIMER_MESSAGE="Timer nicht vorhanden";
+	public final static String TIMER_START_MESSAGE="Timer gestartet";
+	public final static String SHUTDOWN_MESSAGE="Rechner fährt herunter";
+	public final static String NOT_SHUTDOWN_MESSAGE="Rechner wird nicht heruntergefahren";
+	
+}
