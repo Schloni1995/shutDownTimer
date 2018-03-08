90ad9d691ab662404c231574734769d11c2e2c4c
Schloni1995 <33430528+Schloni1995@users.noreply.github.com>
2018-03-09 00:25:20 +0100

    Changed image import

--- Tools/src/constants/Paths.java
+++ Tools/src/constants/Paths.java
@@ -2,6 +2,6 @@ package constants;
 
 public class Paths
 {
-	public final static String BACKGROUND1 = "C:\\Users\\Toni\\git\\shutDownTimer\\Tools\\src\\images\\time-wallpaper-14.jpg";
-	public final static String BACKGROUND2 = "C:\\Users\\Toni\\git\\shutDownTimer\\Tools\\src\\images\\time-wallpaper-9.jpg";
+	public final static String BACKGROUND1 = "recources/images/time-wallpaper-14.jpg";
+	public final static String BACKGROUND2 = "recources/images/time-wallpaper-9.jpg";
 }
