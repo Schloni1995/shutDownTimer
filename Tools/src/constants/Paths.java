80617c0e7ba2f06b5b7b93326981f6c6b0f9105b
Schloni1995 <33430528+Schloni1995@users.noreply.github.com>
2018-03-20 10:20:55 +0100

    nothing

--- Tools/src/constants/Paths.java
+++ Tools/src/constants/Paths.java
@@ -2,6 +2,7 @@ package constants;
 
 public class Paths
 {
-	public final static String BACKGROUND1 = "recources/images/time-wallpaper-14.jpg";
-	public final static String BACKGROUND2 = "recources/images/time-wallpaper-9.jpg";
+	public final static String BACKGROUND1 = "/Tools/src/resources/images/time-wallpaper-14.jpg";
+	public final static String BACKGROUND2 = "/src/resources/images/time-wallpaper-9.jpg";
+	public final static String BACKGROUND3 = "/resources/images/timer.jpg";
 }
--- Tools/src/gui/BackGroundPanel.java
+++ Tools/src/gui/BackGroundPanel.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MediaTracker;
 import java.awt.Toolkit;
+import java.io.File;
+import java.net.URL;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
 
@@ -19,24 +21,30 @@ public class BackGroundPanel extends JPanel
 	public BackGroundPanel()
 	{
 		final MediaTracker mt = new MediaTracker(this);
-		img = Toolkit.getDefaultToolkit()
-				.getImage(getClass().getClassLoader().getResource(Paths.BACKGROUND2).getFile());
-		mt.addImage(img, 0);
-		try
+		
+		File f = new File(this.getClass().getClassLoader().getResource(Paths.BACKGROUND2).getFile());
+
+		if (f.exists())
 		{
-			mt.waitForAll();
-		}
-		catch (final InterruptedException e)
-		{
-			LOG.severe(e.getMessage());
+			img = Toolkit.getDefaultToolkit().getImage(f.getAbsolutePath());
+			mt.addImage(img, 0);
+			try
+			{
+				mt.waitForAll();
+			}
+			catch (final InterruptedException e)
+			{
+				LOG.severe(e.getMessage());
+			}
+
+			int w, h;
+			w = (int) (img.getWidth(null) * 0.45);
+			h = (int) (img.getHeight(null) * 0.45);
+			setPreferredSize(new Dimension(w, h));
+
+			LOG.info("Image-Dimension: " + w + "x" + h);
 		}
 
-		int w, h;
-		w = (int) (img.getWidth(null) * 0.45);
-		h = (int) (img.getHeight(null) * 0.45);
-		setPreferredSize(new Dimension(w, h));
-
-		LOG.info("Image-Dimension: " + w + "x" + h);
 	}
 
 	@Override
